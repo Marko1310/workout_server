3671e585b027aacebab0d1abeedd72a6956a1698
Marko1310 <marko.cabo13@gmail.com>
2023-11-05 13:27:24 +0100

    Create user function update with password hash

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/users.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
@@ -12,7 +13,14 @@ export class UsersService {
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const newUser = this.userRepository.create(createUserDto);
+    const { name, email, password } = createUserDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await this.userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     return this.userRepository.save(newUser);
   }
