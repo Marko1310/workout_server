b245f44844e9a59b28f27cefa74ec4680cb4655a
Marko1310 <marko.cabo13@gmail.com>
2023-11-19 13:25:10 +0100

    Add roles to user table

--- src/entities/users.entity.ts
+++ src/entities/users.entity.ts
@@ -2,6 +2,13 @@ import { WorkoutSplits } from './workout_splits.entity';
 import { Entity, Column, OneToMany } from 'typeorm';
 import { BaseEntity } from '../shared/database/base.entity';
 
+export const Role = {
+  ADMIN: 'admin',
+  USER: 'user',
+} as const;
+
+export type Role = (typeof Role)[keyof typeof Role];
+
 @Entity({ name: 'users' })
 export class Users extends BaseEntity {
   @Column({ type: 'varchar', length: 300, nullable: false })
@@ -17,6 +24,9 @@ export class Users extends BaseEntity {
 
   @Column({ type: 'varchar', length: 300, nullable: false })
   password: string;
+
+  @Column({ type: 'enum', enum: Role, default: Role.USER })
+  role: Role;
 
   @OneToMany(
     () => WorkoutSplits,
