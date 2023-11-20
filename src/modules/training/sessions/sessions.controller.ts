c5c244c3911eb46fb7390b40b6a33fd56f411c04
Marko1310 <marko.cabo13@gmail.com>
2023-11-20 17:25:33 +0100

    Add authorization to session controller

--- src/modules/training/sessions/sessions.controller.ts
+++ src/modules/training/sessions/sessions.controller.ts
@@ -1,20 +1,36 @@
-import { Body, Controller, Param, ParseIntPipe, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { SessionsService } from './sessions.service';
 import { ZodPipe } from 'shared/zod.pipe';
 import { AddSessionsDto, AddSessionsSchema } from './dto/session.dto';
 import { WorkoutExistsPipe } from '@training-modules/workouts/pipes/workoutExist.pipe';
+import { Permission, PermissionGuard } from 'shared/auth/permission.guard';
+import { RequestUser } from '@users-modules/decorator/requestUser.decorator';
+import { RequestUserDto } from '@users-modules/dto/request-user.dto';
 
 @Controller('sessions')
+@UseGuards(PermissionGuard)
 export class SessionsController {
   constructor(private sessionsService: SessionsService) {}
 
   @Post(':workoutId')
+  @Permission('create', 'Sessions')
   async createSession(
     @Body(new ZodPipe(AddSessionsSchema)) addSessionDto: AddSessionsDto,
     @Param('workoutId', ParseIntPipe, WorkoutExistsPipe) workoutId: number,
+    @RequestUser() user: RequestUserDto,
   ) {
     const { exercisesData } = addSessionDto;
-    const session = await this.sessionsService.createSession(exercisesData);
+    const session = await this.sessionsService.createSession(
+      user.id,
+      exercisesData,
+    );
     return session;
   }
 }
--- src/modules/training/sessions/sessions.service.ts
+++ src/modules/training/sessions/sessions.service.ts
@@ -8,10 +8,10 @@ import { ExerciseArrayDto, SetDto } from './dto/session.dto';
 export class SessionsService {
   constructor(
     @InjectRepository(Sessions)
-    private sessions: Repository<Sessions>,
+    private sessionsRepository: Repository<Sessions>,
   ) {}
 
-  async createSession(sessionData: ExerciseArrayDto) {
+  async createSession(userId: number, sessionData: ExerciseArrayDto) {
     const promises = sessionData.flatMap(async (session) => {
       const { exerciseId, sets } = session;
       const lastSession = await this.findLastSession(exerciseId);
@@ -19,12 +19,13 @@ export class SessionsService {
       return await Promise.all(
         sets.map(async (set, index) => {
           const newSession = await this.createNewSession(
+            userId,
             exerciseId,
             index,
             set,
             lastSession,
           );
-          return this.sessions.save(newSession);
+          return this.sessionsRepository.save(newSession);
         }),
       );
     });
@@ -33,19 +34,21 @@ export class SessionsService {
   }
 
   private async findLastSession(exerciseId: number) {
-    return this.sessions.findOne({
+    return this.sessionsRepository.findOne({
       where: { exercises: { id: exerciseId } },
       order: { week: 'DESC' },
     });
   }
 
   private async createNewSession(
+    userId: number,
     exerciseId: number,
     index: number,
     set: SetDto,
     lastSession: SetDto,
   ) {
-    return this.sessions.create({
+    return this.sessionsRepository.create({
+      users: { id: userId },
       exercises: { id: exerciseId },
       set: index + 1,
       weight: set.weight,
