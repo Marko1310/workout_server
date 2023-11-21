3c8f0ada03e0daead65f6d7978609fbc6724b4fa
Marko1310 <marko.cabo13@gmail.com>
2023-11-21 14:02:05 +0100

    Remove unused module

--- src/modules/training/sessions/sessions.module.ts
+++ src/modules/training/sessions/sessions.module.ts
@@ -3,12 +3,11 @@ import { SessionsController } from './sessions.controller';
 import { SessionsService } from './sessions.service';
 import { Sessions } from '@entities/sessions.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Exercises } from '@entities/exercises.entity';
 import { Workouts } from '@entities/workouts.entity';
 
 @Module({
   controllers: [SessionsController],
   providers: [SessionsService],
-  imports: [TypeOrmModule.forFeature([Sessions, Exercises, Workouts])],
+  imports: [TypeOrmModule.forFeature([Sessions, Workouts])],
 })
 export class SessionsModule {}
