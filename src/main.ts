bd3cdf4c3803102b26436dfb465dd457393674e2
Marko1310 <marko.cabo13@gmail.com>
2023-12-03 16:43:02 +0100

    Implement cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
 import * as cookieParser from 'cookie-parser';
 import { ZodExceptionFilter } from 'shared/zod-exception.filter';
+// import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -13,7 +14,7 @@ async function bootstrap() {
   const logger = app.get(Logger);
   const config = app.get(ConfigService);
   const port = config.get<number>('app.port');
-
+  app.enableCors();
   app.use(cookieParser());
 
   app.useLogger(logger);
