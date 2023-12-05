a2e13e31cbba2028a524db9a4f48852fa675fd15
Marko1310 <marko.cabo13@gmail.com>
2023-12-05 10:51:07 +0100

    Improve signupDto

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 export const SignupDtoSchema = z.object({
-  name: z.string(),
+  name: z.string().min(1),
   email: z.string().email(),
-  password: z.string(),
+  password: z.string().min(6),
 });
 
 export type SignupDto = z.infer<typeof SignupDtoSchema>;
