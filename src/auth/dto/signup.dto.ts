2d25f94b5efb0be0f69b28697338014afd78f705
Marko1310 <marko.cabo13@gmail.com>
2023-12-05 12:18:47 +0100

    Add custom error messages signupDto

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 export const SignupDtoSchema = z.object({
-  name: z.string().min(1),
-  email: z.string().email(),
-  password: z.string().min(6),
+  name: z.string().min(1, 'Name must be at least 1 character long'),
+  email: z.string().email('Email field can not be empty'),
+  password: z.string().min(6, 'Password must be at least 6 characters long'),
 });
 
 export type SignupDto = z.infer<typeof SignupDtoSchema>;
